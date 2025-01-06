19cb8d4291d7605c47b56305608b829f2ca6b44a
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-06 16:49:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var isDate = function (input) {
   // Match a date in the format YYYY-MM-DD
-  return /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/.test(input);
+  // return /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/.test(input);
+	return !isNaN(Date.parse(input));
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+// const input = prompt("Enter Date.");
 alert(isDate(input));
